bdcdb61bf97a9fbf4a4d7590a1ffa2c3b1173607
unknown <nocoop527@gmail.com>
2022-06-12 00:38:08 +0700

    0.1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 var compression = require("compression")
 var helmet = require("helmet")
 const cors = require("cors")
+// require('dotenv').config()
 
 var admin = require('firebase-admin')
 const { initializeApp } = require("firebase-admin/app")
@@ -20,13 +21,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+
 const run = async (db) => {
-    // initializeApp({
-    //     credential: admin.credential.applicationDefault(),
-    //     databaseURL: 'https://emergency-notifier-f30fa-default-rtdb.firebaseio.com'
-    // })
-
-
+    initializeApp({
+        credential: admin.credential.cert(
+            JSON.parse(process.env.SERVICE_ACCOUNT_JSON)
+        ),
+        databaseURL: 'https://emergency-notifier-f30fa-default-rtdb.firebaseio.com'
+    })
 
     const PORT = process.env.PORT || 8080
 
